7b0f0d29c34595a1bd2b128311abb180186f8185
jiangkui <2227847330@qq.com>
2024-12-17 21:07:36 +0800

    feature: remove unused component

--- app/src/main/java/cn/losefeather/compose/mine/MinePage.kt
+++ app/src/main/java/cn/losefeather/compose/mine/MinePage.kt
@@ -1,10 +1,6 @@
 package cn.losefeather.compose.mine
-
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 
 @Composable
 fun MinePage() {
-    Text("mine")
 }
